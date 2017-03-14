451749924548687b54660d35df73c0e306d70d8d
Anastasia Trofimova <nastasiatm@gmail.com>
2017-03-14 20:29:36 +0300

    fix libraries

--- src/test/FunctionTest.java
+++ src/test/FunctionTest.java
@@ -1,17 +1,18 @@
 /**
  * Created by anastasia on 12.03.17.
  */
-
 import org.junit.Test;
-import junit.framework.Assert.*;
 
-public class Test {
+import static org.junit.Assert.assertEquals;
 
-    private void testMultiply() {
+public class FunctionTest {
+
+    @Test
+    public void testMultiply() {
         // Тестируемый класс
         Function tester = new Function();
 
         // Проверяемый метод
-        assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+        assertEquals("10 x 5 must be 50", 15, tester.calA(10, 5));
     }
 }
